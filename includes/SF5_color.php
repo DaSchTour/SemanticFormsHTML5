cb34d94905d59d67ebb8eace9aee5b9379bd23d5
DaSch <dasch@daschmedia.de>
2012-07-23 15:03:57 +0200

    checkt

--- includes/SF5_color.php
+++ includes/SF5_color.php
@@ -13,7 +13,14 @@ class SF5color extends SFFormInput {
 		return $params;
 	}
 	
-	public function getHtmlText() {
+	public function getHtmlText($cur_value, $input_name, $is_mandatory, $is_disabled, $other_args) {
+		global $sfgTabIndex, $sfgFieldNum, $sfgShowOnSelect;
+
+		if ( array_key_exists( 'class', $other_args ) ) {
+			$className .= ' ' . $other_args['class'];
+		}
+		$input_id = "input_$sfgFieldNum";
+		$disabled_text = ( $is_disabled ) ? 'disabled' : '';
 		$html = <<<END
 	<input id="$input_id" name="{$input_name}[value]" type="color" class="$className" tabindex="$sfgTabIndex" $checked_str $disabled_text/>
 END;
