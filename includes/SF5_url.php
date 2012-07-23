44d17eb4167469f460dcd34bfbd8789891766aa9
DaSch <dasch@daschmedia.de>
2012-07-23 15:58:14 +0200

    added url even it does not work

--- includes/SF5_url.php
+++ includes/SF5_url.php
@@ -1,3 +1,26 @@
 <?php
 
 /* HTML5 url */
+
+class SF5url extends SFFormInput {
+
+	public static function getName() {
+		return 'html5url';
+    }
+	
+	public static function getParameters() {
+		$params = parent::getParameters();
+		return $params;
+	}
+	
+	public function getHtmlText() {
+		$html = <<<END
+	<input id="$input_id" name="{$input_name}[value]" type="url" class="$className" tabindex="$sfgTabIndex" $checked_str $disabled_text/>
+END;
+		return $html;
+	}
+	
+	public static function getOtherPropTypesHandled() {
+		return array('url');
+	}
+}
