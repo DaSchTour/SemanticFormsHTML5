8ce12489e079225f2ff104c2b6711dae58a14fb0
DaSch <dasch@daschmedia.de>
2012-07-23 16:25:10 +0200

    Revert "try it simple"
    This reverts commit 07934b90f7aaa6dbbcd16aa3271781fddaef7ae5.

--- SemanticFormsHTML5.php
+++ SemanticFormsHTML5.php
@@ -16,11 +16,28 @@ $dir = dirname( __FILE__ ) . '/';
 $HTML5inputs = array('button','checkbox','color','date','datetime','datetime_local','email','file','hidden','image','month','number','password','radio','range','reset','search','submit','tel','text','time','url','week' );
 
 $wgExtensionMessagesFiles['SemanticFormsHTML5'] = $dir . 'SemanticFormsHTML5.i18n.php';
+$wgHooks['ParserFirstCallInit'][] = 'wfSF5Setup';
 
+/*
+foreach ($HTML5inputs as $key => $value) {
+	$wgAutoloadClasses['SF5'.$value] = $dir . 'includes/SF5_'.$value.'.php';
+}
+
+function wfSF5Setup() {
+        global $sfgFormPrinter, $wgVersion, $HTML5inputs;
+ 		foreach ($HTML5inputs as $key => $value) {
+			$sfgFormPrinter->registerInputType('SF5'.$value);
+		}
+		return true;
+}
+ * */
+ 
 global $sfgFormPrinter;
-$sfgFormPrinter->setInputTypeHook('html5', 'sfHtml5', array());
+foreach ($HTML5inputs as $key => $value) {
+	$sfgFormPrinter->setInputTypeHook('html5'.$key, 'sfHtml5'.$key, array());
+}
 
-function sfHtml5($cur_value, $input_name, $is_mandatory, $is_disabled, $other_args) {
+function sfHtml5color($cur_value, $input_name, $is_mandatory, $is_disabled, $other_args) {
 		global $sfgTabIndex, $sfgFieldNum, $sfgShowOnSelect;
 
 		if ( array_key_exists( 'class', $other_args ) ) {
@@ -28,9 +45,34 @@ function sfHtml5($cur_value, $input_name, $is_mandatory, $is_disabled, $other_ar
 		}
 		$input_id = "input_$sfgFieldNum";
 		$disabled_text = ( $is_disabled ) ? 'disabled' : '';
-		$html5input=$field_args['html5'];
 		$html = <<<END
-	<input id="$input_id" name="{$input_name}[value]" type="$html5input" class="$className" tabindex="$sfgTabIndex" $checked_str $disabled_text/>
+	<input id="$input_id" name="{$input_name}[value]" type="color" class="$className" tabindex="$sfgTabIndex" $checked_str $disabled_text/>
+END;
+		return $html;
+	}
+function sfHtml5number($cur_value, $input_name, $is_mandatory, $is_disabled, $other_args) {
+		global $sfgTabIndex, $sfgFieldNum, $sfgShowOnSelect;
+
+		if ( array_key_exists( 'class', $other_args ) ) {
+			$className .= ' ' . $other_args['class'];
+		}
+		$input_id = "input_$sfgFieldNum";
+		$disabled_text = ( $is_disabled ) ? 'disabled' : '';
+		$html = <<<END
+	<input id="$input_id" name="{$input_name}[value]" type="number" class="$className" tabindex="$sfgTabIndex" $checked_str $disabled_text/>
+END;
+		return $html;
+	}
+function sfHtml5email($cur_value, $input_name, $is_mandatory, $is_disabled, $other_args) {
+		global $sfgTabIndex, $sfgFieldNum, $sfgShowOnSelect;
+
+		if ( array_key_exists( 'class', $other_args ) ) {
+			$className .= ' ' . $other_args['class'];
+		}
+		$input_id = "input_$sfgFieldNum";
+		$disabled_text = ( $is_disabled ) ? 'disabled' : '';
+		$html = <<<END
+	<input id="$input_id" name="{$input_name}[value]" type="email" class="$className" tabindex="$sfgTabIndex" $checked_str $disabled_text/>
 END;
 		return $html;
 	}
